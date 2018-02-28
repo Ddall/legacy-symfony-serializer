ec25ee94162c500a29b7bd29d0ca497ab8186dbe
Allan <allan.irdel@gmail.com>
2018-02-28 14:03:23 +0100

    first commit

---
+++ Normalizer/SerializerAwareNormalizer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Legacy\Symfony\Component\Serializer\Normalizer;
+
+use Legacy\Symfony\Component\Serializer\SerializerInterface;
+use Legacy\Symfony\Component\Serializer\SerializerAwareInterface;
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * SerializerAware Normalizer implementation
+ *
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+abstract class SerializerAwareNormalizer implements SerializerAwareInterface, NormalizerInterface
+{
+    protected $serializer;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSerializer(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+Symfony's Serializer Component v2.0
+===================================
+
+This bundle is a copy of the v2.0 branch from https://github.com/symfony/serializer where all namespaces have been altered from
+ ``Symfony\Component\Serializer`` to  ``Legacy\Symfony\Component\Serializer``
+
+**USE AT YOUR OWN RISK, THIS BUNDLE'S CODE HAS BEEN UNMAINTAINED SINCE 2012.**
+
+Serializer Component
+====================
+
+With the Serializer component its possible to handle serializing data structures,
+including object graphs, into array structures or other formats like XML and JSON.
+It can also handle deserializing XML and JSON back to object graphs.
+
+Resources
+---------
+
+Unit tests:
+
+https://github.com/symfony/symfony/tree/master/tests/Symfony/Tests/Component/Serializer
